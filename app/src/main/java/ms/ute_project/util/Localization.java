30fc7a59dbc4535290483a0f7475cd4ff04f6d9a
Amberladys <masobiech@wp.pl>
2015-02-13 20:31:42 +0100

    z funkcją

--- app/src/main/java/ms/ute_project/util/Localization.java
+++ app/src/main/java/ms/ute_project/util/Localization.java
@@ -29,7 +29,47 @@ public class Localization extends AsyncTask<Void, Void, String> {
     }
 
 
+    protected String doInBackground(Void... arg0) {
 
+        String url = "https://api.bihapi.pl/rest/terminal_location/location?query=%7B%22acceptableAccuracy%22%3A%22100%22%2C%22address%22%3A%22tel%3A48514168461%22%2C%22requestedAccuracy%22%3A%22100%22%2C%22tolerance%22%3A%22NoDelay%22%7D";
+        String coord = null;
+
+        String TAG_DATA = "result";
+        String TAG_COORDINATES_LAT = "latitude";
+        String TAG_COORDINATES_LON = "longitude";
+
+
+        HttpRetriver sh = new HttpRetriver();
+
+        // Making a request to url and getting response
+        String jsonStr = sh.makeServiceCall(url, HttpRetriver.GET);
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+
+                // Getting JSON Array node
+                JSONObject data = jsonObj.getJSONObject(TAG_DATA);
+                // geometry node is JSON Object
+
+                String latitude = data.getString(TAG_COORDINATES_LAT);
+                String longitude = data.getString(TAG_COORDINATES_LON);
+
+                coord.concat(longitude);
+                coord.concat(",");
+                coord.concat(latitude);
+                coord.concat(",");
+
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            Log.e("ServiceHandler", "Couldn't get any data from the url");
+        }
+        return coord;
+
+    }
 
     protected void onPostExecute(String result) {
 
